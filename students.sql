234b938d670f26474659f266ed67dad52112986a
erinngentry <43663396+erinngentry@users.noreply.github.com>
2020-04-12 15:11:25 -0500

    Modified file to only display student table

--- students.sql
+++ students.sql
@@ -1,41 +1,40 @@
-DROP TABLE IF EXISTS `STUDENT`;
-CREATE TABLE `STUDENT` (
+DROP TABLE IF EXISTS `STUDENTS`;
+CREATE TABLE `STUDENTS` (
     `StudentID` int(11) NOT NULL AUTO_INCREMENT,
     `FirstName` varchar(255) DEFAULT NULL,
     `LastName` varchar(255) DEFAULT NULL,
     `Major` varchar(255) DEFAULT NULL,
     PRIMARY KEY (`StudentID`)
-) ENGINE=InnoDB;
-
-INSERT INTO `STUDENT` (FirstName, LastName, Major)
+);
+ 
+/* STUDENT INFORMATION */
+INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('Harriet', 'Tubman', 'Computer Science');
-INSERT INTO `STUDENT` (FirstName, LastName, Major)
+INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('Langston', 'Hughes', 'Computer Science');
-INSERT INTO `STUDENT` (FirstName, LastName, Major)
+INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('Fredrick', 'Douglass', 'Computer Science');
-INSERT INTO `STUDENT` (FirstName, LastName, Major)
+INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('Michael', 'Jackson', 'Computer Science');
-INSERT INTO `STUDENT` (FirstName, LastName, Major)
+INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('Janet', 'Jackson', 'Computer Science');
-INSERT INTO `STUDENT` (FirstName, LastName, Major)
+INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('George', 'Carver', 'Computer Science');
-INSERT INTO `STUDENT` (FirstName, LastName, Major)
+INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('Maya', 'Angelou', 'Computer Science');
-INSERT INTO `STUDENT` (FirstName, LastName, Major)
+INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('Shirley', 'Chisholm', 'Computer Science');
-INSERT INTO `STUDENT` (FirstName, LastName, Major)
+INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('Martin', 'King', 'Computer Science');
-INSERT INTO `STUDENT` (FirstName, LastName, Major)
+INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('Duke', 'Ellington', 'Computer Science');
-INSERT INTO `STUDENT` (FirstName, LastName, Major)
+INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('Mae', 'Jemison', 'Computer Science');
-INSERT INTO `STUDENT` (FirstName, LastName, Major)
+INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('Zora', 'Hurston', 'Computer Science');
-INSERT INTO `STUDENT` (FirstName, LastName, Major)
+INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('Toni', 'Morrison', 'Computer Science');
-INSERT INTO `STUDENT` (FirstName, LastName, Major)
+INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('Ida', 'Wells', 'Computer Science');
-INSERT INTO `STUDENT` (FirstName, LastName, Major)
+INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('Alain', 'Locke', 'Computer Science');
-
-COMMIT;
