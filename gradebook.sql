90ddbe9f8189cfae5731ff5d283b05e98a6c740b
erinngentry <43663396+erinngentry@users.noreply.github.com>
2020-04-12 14:33:16 -0500

    Added values to Students Table

--- gradebook.sql
+++ gradebook.sql
@@ -1,8 +1,41 @@
 DROP TABLE IF EXISTS `STUDENT`;
 CREATE TABLE `STUDENT` (
+    `StudentID` int(11) NOT NULL AUTO_INCREMENT,
     `FirstName` varchar(255) DEFAULT NULL,
     `LastName` varchar(255) DEFAULT NULL,
     `Major` varchar(255) DEFAULT NULL,
-    `StudentID` int(11) NOT NULL AUTO_INCREMENT,
     PRIMARY KEY (`StudentID`)
 ) ENGINE=InnoDB;
+
+INSERT INTO `STUDENT` (FirstName, LastName, Major)
+VALUES ('Harriet', 'Tubman', 'Computer Science');
+INSERT INTO `STUDENT` (FirstName, LastName, Major)
+VALUES ('Langston', 'Hughes', 'Computer Science');
+INSERT INTO `STUDENT` (FirstName, LastName, Major)
+VALUES ('Fredrick', 'Douglass', 'Computer Science');
+INSERT INTO `STUDENT` (FirstName, LastName, Major)
+VALUES ('Michael', 'Jackson', 'Computer Science');
+INSERT INTO `STUDENT` (FirstName, LastName, Major)
+VALUES ('Janet', 'Jackson', 'Computer Science');
+INSERT INTO `STUDENT` (FirstName, LastName, Major)
+VALUES ('George', 'Carver', 'Computer Science');
+INSERT INTO `STUDENT` (FirstName, LastName, Major)
+VALUES ('Maya', 'Angelou', 'Computer Science');
+INSERT INTO `STUDENT` (FirstName, LastName, Major)
+VALUES ('Shirley', 'Chisholm', 'Computer Science');
+INSERT INTO `STUDENT` (FirstName, LastName, Major)
+VALUES ('Martin', 'King', 'Computer Science');
+INSERT INTO `STUDENT` (FirstName, LastName, Major)
+VALUES ('Duke', 'Ellington', 'Computer Science');
+INSERT INTO `STUDENT` (FirstName, LastName, Major)
+VALUES ('Mae', 'Jemison', 'Computer Science');
+INSERT INTO `STUDENT` (FirstName, LastName, Major)
+VALUES ('Zora', 'Hurston', 'Computer Science');
+INSERT INTO `STUDENT` (FirstName, LastName, Major)
+VALUES ('Toni', 'Morrison', 'Computer Science');
+INSERT INTO `STUDENT` (FirstName, LastName, Major)
+VALUES ('Ida', 'Wells', 'Computer Science');
+INSERT INTO `STUDENT` (FirstName, LastName, Major)
+VALUES ('Alain', 'Locke', 'Computer Science');
+
+COMMIT;
