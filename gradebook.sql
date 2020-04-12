2f2056432d230306bb55be3941eac82819b38403
erinngentry <43663396+erinngentry@users.noreply.github.com>
2020-04-12 15:24:29 -0500

    Merged table information to one file

--- gradebook.sql
+++ gradebook.sql
@@ -6,6 +6,17 @@ CREATE TABLE `STUDENTS` (
     `Major` varchar(255) DEFAULT NULL,
     PRIMARY KEY (`StudentID`)
 );
+
+DROP TABLE IF EXISTS `COURSES`; 
+CREATE TABLE `COURSES` (
+    `CourseID` int(11) NOT NULL AUTO_INCREMENT,
+    `CourseName` varchar(255) DEFAULT NULL,
+    `Department` varchar(255) DEFAULT NULL,
+    `CourseNumber` varchar(255) DEFAULT NULL,
+    `Semester` varchar(255) DEFAULT NULL,
+    `SchoolYear` int(4) DEFAULT NULL,
+    PRIMARY KEY (`CourseID`)
+ );
  
 /* STUDENT INFORMATION */
 INSERT INTO `STUDENTS` (FirstName, LastName, Major)
@@ -38,3 +49,15 @@ INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('Ida', 'Wells', 'Computer Science');
 INSERT INTO `STUDENTS` (FirstName, LastName, Major)
 VALUES ('Alain', 'Locke', 'Computer Science');
+
+/* COURSE INFORMATION */
+INSERT INTO `COURSES` (CourseID, CourseName, Department, CourseNumber, Semester, SchoolYear)
+VALUES (CourseID, 'Database Systems', 'CSCI', '432', 'Spring', 2020);
+INSERT INTO `COURSES` (CourseID, CourseName, Department, CourseNumber, Semester, SchoolYear)
+VALUES (CourseID, 'Computer Science II', 'CSCI', '136', 'Spring', 2020);
+INSERT INTO `COURSES` (CourseID, CourseName, Department, CourseNumber, Semester, SchoolYear)
+VALUES (CourseID, 'Software Engineering', 'CSCI', '375', 'Fall', 2019);
+INSERT INTO `COURSES` (CourseID, CourseName, Department, CourseNumber, Semester, SchoolYear)
+VALUES (CourseID, 'Intro to Linear Algebra', 'MATH', '001', 'Spring', 2020);
+INSERT INTO `COURSES` (CourseID, CourseName, Department, CourseNumber, Semester, SchoolYear)
+VALUES (CourseID, 'Spanish I', 'SPAN', '001', 'Fall', 2019);
