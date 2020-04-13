776c31a9d1098df87ee8d2b706a74606ddf1afc0
erinngentry <43663396+erinngentry@users.noreply.github.com>
2020-04-13 01:35:24 -0500

    Edited Distribution query

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 select * from `distribution`;
 select * from `courses`;
-Select * From `distribution`
-WHERE ("Courses.CourseID" = "Distribution.CourseID");
+select * from `distribution`
+inner join `courses` on (Courses.CourseID = Distribution.CourseID);
 
 select * from `enrollment`
 inner join `students` on (Students.StudentID = Enrollment.StudentID)
