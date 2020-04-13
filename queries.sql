e6b4cb4565cef3acc47e71addb1ae8c1876ea010
JCameron17 <54552570+JCameron17@users.noreply.github.com>
2020-04-13 18:00:38 -0400

    Update queries.sql
    compute average/highest/lowest score of assignments

--- queries.sql
+++ queries.sql
@@ -21,3 +21,9 @@ left join `COURSES` on (COURSES.CourseID = DISTRIBUTION.courseID);
 select * from `STUDENTGRADES`
 inner join `STUDENTS` on (STUDENTS.studentID = STUDENTGRADES.studentID)
 inner join `ASSIGNMENTS` on (ASSIGNMENTS.assignID = STUDENTGRADES.assignID);
+
+-- query: Compute the average/highest/lowest score of an assignment
+select a.AssignmentID, avg(s.POINTS), max(s.POINTS), min(s.POINTS) 
+from ASSIGNMENTS a, SCORE s 
+where a.AssignmentID=2 AND s.AssignmentID=a.AssignmentID;
+
