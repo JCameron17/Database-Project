28775eb328a9ae5ea81b59f85cab3a12b01dd726
erinngentry <43663396+erinngentry@users.noreply.github.com>
2020-04-13 01:48:06 -0500

    Added Assignments query

--- queries.sql
+++ queries.sql
@@ -6,3 +6,6 @@ inner join `courses` on (Courses.CourseID = Distribution.CourseID);
 select * from `enrollment`
 inner join `students` on (Students.StudentID = Enrollment.StudentID)
 inner join `courses` on (Courses.CourseID = Enrollment.CourseID);
+
+select * from `assignments`
+inner join `distribution` on (distribution.distribID = assignments.distribID);
