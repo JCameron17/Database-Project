1a3b9e108a0cf7101cca7cfe4d64a936349173e1
erinngentry <43663396+erinngentry@users.noreply.github.com>
2020-04-13 15:23:19 -0500

    Specifying queries

--- queries.sql
+++ queries.sql
@@ -1,14 +1,21 @@
 select * from `distribution`;
 select * from `courses`;
-select * from `distribution`
-inner join `courses` on (Courses.CourseID = Distribution.CourseID);
+
+-- query: distribution by course
+select Courses.CourseName, Type, Percent  from `distribution`, `courses`
+where (courses.courseID = distribution.CourseID)
+order by CourseName;
+
 
 select * from `enrollment`
 inner join `students` on (Students.StudentID = Enrollment.StudentID)
 inner join `courses` on (Courses.CourseID = Enrollment.CourseID);
 
-select * from `assignments`
-inner join `distribution` on (distribution.distribID = assignments.distribID);
+-- query: assignments by distribution
+select AssignID, Courses.CourseName, Instance, MaxPoints, Distribution.Type
+from `assignments`
+left join `distribution` on (distribution.distribID = assignments.distribID)
+left join `courses` on (Courses.CourseID = distribution.courseID);
 
 select * from `studentgrades`
 inner join `students` on (students.studentID = studentgrades.studentID)
