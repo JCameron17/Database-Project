b31f32eac655b1772c88b2ff23f8ddd483d931b7
erinngentry <43663396+erinngentry@users.noreply.github.com>
2020-04-13 01:34:21 -0500

    Added Enrollment query

--- queries.sql
+++ queries.sql
@@ -2,3 +2,7 @@ select * from `distribution`;
 select * from `courses`;
 Select * From `distribution`
 WHERE ("Courses.CourseID" = "Distribution.CourseID");
+
+select * from `enrollment`
+inner join `students` on (Students.StudentID = Enrollment.StudentID)
+inner join `courses` on (Courses.CourseID = Enrollment.CourseID);
