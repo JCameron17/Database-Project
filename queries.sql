8fa09e4af04984c553ef042c6865c035081ab793
JCameron17 <54552570+JCameron17@users.noreply.github.com>
2020-04-13 22:04:16 -0400

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -13,14 +13,14 @@ left join `DISTRIBUTION` on (DISTRIBUTION.distribID = ASSIGNMENTS.distribID)
 left join `COURSES` on (COURSES.CourseID = DISTRIBUTION.courseID);
 
 -- query: student grades by assignment
-select STUDENTGRADES.AssignID, STUDENTGRADES.StudentID, FirstName, LastName, Points, MaxPoints, Instance
-from `STUDENTGRADES`
-right JOIN `ASSIGNMENTS` ON (ASSIGNMENTS.AssignID = STUDENTGRADES.AssignID)
-LEFT JOIN `STUDENTS` ON (STUDENTS.StudentID = STUDENTGRADES.StudentID);
+select SCORE.AssignID, SCORE.StudentID, FirstName, LastName, Points, MaxPoints, Instance
+from `SCORE`
+right JOIN `ASSIGNMENTS` ON (ASSIGNMENTS.AssignID = SCORE.AssignID)
+LEFT JOIN `STUDENTS` ON (STUDENTS.StudentID = SCORE.StudentID);
 
 -- #4 query: Compute the average/highest/lowest score of an assignment
 select a.AssignID, a.MaxPoints, avg(s.POINTS) AS PointAVG, max(s.POINTS) AS BestScore, min(s.POINTS) AS WorstScore
-from ASSIGNMENTS a, STUDENTGRADES s 
+from ASSIGNMENTS a, SCORE s
 where a.AssignID=2 AND s.AssignID=a.AssignID;
 
 -- #5 query: List all of the students in a given course
@@ -33,8 +33,8 @@ order by LastName asc;
 -- #6 query: List all of the students in a course and all of their scores on every assignment
 select LastName, FirstName, CourseName, ASSIGNMENTS.AssignID, Points, MaxPoints from `ENROLLMENT`
 inner join `STUDENTS` on (STUDENTS.StudentID = ENROLLMENT.StudentID)
-inner join `STUDENTGRADES` on (STUDENTGRADES.StudentID = ENROLLMENT.StudentID)
-inner join `ASSIGNMENTS` on (ASSIGNMENTS.AssignID = STUDENTGRADES.AssignID)
+inner join `SCORE` on (SCORE.StudentID = ENROLLMENT.StudentID)
+inner join `ASSIGNMENTS` on (ASSIGNMENTS.AssignID = SCORE.AssignID)
 inner join `COURSES` on (COURSES.CourseID = ENROLLMENT.CourseID) where (COURSES.CourseID = 1)
 order by AssignID, LastName asc;
 
@@ -47,9 +47,8 @@ FROM (
     AND STUDENT.StudentID = SCORE.StudentID) st
 JOIN
 (SELECT StudentID, CourseID, CategoryName, ASSIGNMENTS.AssignmentID, Points, ASSIGNMENTS.PointsPossible, DISTRIBUTION.Percent
-    FROM DISTRIBUTION JOIN ASSIGNMENTS JOIN STUDENTGRADES
+    FROM DISTRIBUTION JOIN ASSIGNMENTS JOIN SCORE
     WHERE DISTRIBUTION.DistributionID = ASSIGNMENTS.DistributionID
-    AND ASSIGNMENTS.AssignmentID = STUDENTGRADES.AssignmentID) pt
+    AND ASSIGNMENTS.AssignmentID = SCORE.AssignmentID) pt
 WHERE st.AssignmentID = pt.AssignmentID
 AND st.Points = pt.Points AND st.StudentID=1234;
-
